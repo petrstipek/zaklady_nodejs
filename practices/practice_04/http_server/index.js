9456c1568612ccc4e7a05dfba3df406f71bbfd7d
petrstipek <petr@stip.cz>
2026-03-16 17:47:40 +0100

    practice: code from practice 04

--- practices/practice_04/http_server/index.js
+++ practices/practice_04/http_server/index.js
@@ -1,13 +1,30 @@
 import http from "http"
+import fs from "fs/promises"
+import { read } from "fs"
 
-const server = http.createServer((request, response) => {
-    console.log(request.url)
+const readHtml = async (file) => {
+    //await new Promise((resolve) => setTimeout(resolve, 2000)) // waiting for 2s
+    try {
+        const data = await fs.readFile(file)
+        return data.toString()
+    } catch (err){
+        console.log("file does not exist", err)
+        return null;
+    }
+}
 
-    //response.statusCode = 404
-    response.setHeader("Content-type", "text/html")
-    //response.statusCode = 301
-    //response.setHeader("Location", "/test")
-    response.write("Hello, World! bla bla")
+const server = http.createServer(async (request, response) => {
+    let html = await readHtml("public/" + request.url)
+    console.log(html)
+    
+    if (html){
+        response.statusCode = 200
+    } else {
+        html = await readHtml("not_found.html")
+        response.statusCode = 404
+    }
+
+    response.write(html)
     response.end()
 })
 
